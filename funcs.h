78cb2b35f066581192e7325261a53a9e9c2f3ace
Halil Akca <halilakca321@gmail.com>
2022-09-08 00:36:06 -0400

    added header file with print_interval() included

---
+++ funcs.h
@@ -0,0 +1,12 @@
+/*
+        Author: Halil Akca
+        Course: CSCI-135
+        Instructor: Mike Zamansky
+        Assignment: Lab 02, Task B
+
+        This file serves as a means to let one file use the contents of another file.
+*/
+#include <iostream>
+#pragma once
+
+void print_interval(int L, int U);
